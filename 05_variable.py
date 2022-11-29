728dbf7bb2a7fe5b98bd2e43b9a8b701b6ae897f
amirasghar41 <119133182+amirasghar41@users.noreply.github.com>
2022-11-30 00:18:35 +0500

    variable

---
+++ 05_variable.py
@@ -0,0 +1,23 @@
+x=5
+print(x)
+y=("My name is muhammad Aamir")
+print(y)
+x=x+45
+print(x)
+z=56.7
+print(z)
+b=True
+print(b)
+# types of variables
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(b))
+
+fruit_basket="Mangoes","Orangies"
+print(fruit_basket)
+print(type(fruit_basket))
+fruit_basket="Mangoes,Orangies"
+print(fruit_basket)
+print(type(fruit_basket))
