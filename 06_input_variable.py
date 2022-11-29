e7444eccb22a3143f80530b6cff316a5c77cac91
amirasghar41 <119133182+amirasghar41@users.noreply.github.com>
2022-11-30 00:19:16 +0500

    input_variable

---
+++ 06_input_variable.py
@@ -0,0 +1,12 @@
+
+
+
+# fruit_basket=input("Your best favorite fluit is? ")
+# print(fruit_basket)
+# name=input("What is your name is? ")
+# greeting="hello!"
+# print(greeting,name)
+name=input("What is your name is? ")
+age=input("How old are you ")
+greeting="Hello!"
+print(greeting,name," ,You are still young.")
